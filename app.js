06180ae73541ae15b7c0f8987b40f8ad0290e0dd
IT E-Channel <kebhanaindonesia@gmail.com>
2023-01-26 13:51:03 +0700

    create pagination for /recipes endpoint

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3003
 route = require('./routes')
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.use(route)
 
 app.listen(port, () => {
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const recipes = require('../recipes.json')
 
 router.get('/', (req, res) => {
-    res.send('recipes page')
+    //limit = selisih start slice dengan end
+    //page = slice end didapat dari page * limit
+    // console.log(req.query.page, req.query.limit);
+    let end = 3;
+    let start = 0
+    let page = req.query.page;
+    let limit = req.query.limit;
+    if (!limit) limit = 3;
+    if (page) {
+        end = page * limit;
+        start = end - limit;
+    }
+    // let output = recipes.slice(3, 6)
+    let output = recipes.slice(start, end)
+    res.status(200).json(output)
 })
 
 module.exports = router
