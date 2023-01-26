e8c86c9296654df6b199a2667b50daa69ce30892
IT E-Channel <kebhanaindonesia@gmail.com>
2023-01-26 13:54:17 +0700

    removing comments

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,7 +4,6 @@ const recipes = require('../recipes.json')
 router.get('/', (req, res) => {
     //limit = selisih start slice dengan end
     //page = slice end didapat dari page * limit
-    // console.log(req.query.page, req.query.limit);
     let end = 3;
     let start = 0
     let page = req.query.page;
@@ -14,7 +13,6 @@ router.get('/', (req, res) => {
         end = page * limit;
         start = end - limit;
     }
-    // let output = recipes.slice(3, 6)
     let output = recipes.slice(start, end)
     res.status(200).json(output)
 })
